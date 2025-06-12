36f72166df101cd2d67584ddff0184825522d20d
QuadCore <architchoudharybcs@gmail.com>
2025-06-12 21:28:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
 
 // Middleware
 app.use(cors());
